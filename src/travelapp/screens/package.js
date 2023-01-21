7e89515b8e492af9a2ea739c887f514f0b541c81
empiretylh <thuralinhtut3@gmail.com>
2023-01-21 11:15:49 +0630

    packages

--- src/travelapp/screens/package.js
+++ src/travelapp/screens/package.js
@@ -9,7 +9,7 @@ import {
 	Button,
 	Modal,
 } from "react-bootstrap";
-import { PersonAdd } from "react-bootstrap-icons";
+import { PersonAdd, Search } from "react-bootstrap-icons";
 
 import services from "../data/services";
 import { TokenContext, LoadingContext } from "../context/Context";
@@ -17,11 +17,24 @@ import { useMutation } from "react-query";
 import { IMAGE } from "../../assets/assets";
 
 const Package = () => {
-	
-
 	return (
-		<div className={"pages addadmin"}>
-				<h4>Packages</h4>
+		<div className={"pages"}>
+			<div style={{ display:'flex',justifyContent:'center',alignItems:'center',padding: 10,backgroundColor: 'red' }}>
+				<InputGroup style={{width: 500}}>
+					<Form.Control
+						type="text"
+						placeholder="Search Packages"
+						className="searchfield"
+					/>
+					<Button>
+						<Search size={20} style={{ marginLeft: 3 }} />
+					</Button>
+				</InputGroup>
+			</div>
+			<div>
+				<h1>Add New Package</h1>
+				<Button variant='primary'>Add New Package</Button>
+			</div>
 		</div>
 	);
 };
