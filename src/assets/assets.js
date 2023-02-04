c28ec76a46a1e9327546b7ff5b9eeec9a8c4d71d
empiretylh <thuralinhtut3@gmail.com>
2023-02-04 10:55:23 +0630

    commit

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,10 +1,7 @@
 export const IMAGE = {
-  king: require("./voting/crown.png"),
-  queen: require("./voting/queen_crown.png"),
-  loading: require("./voting/spinnerloading.gif"),
+ 
+  loading: require("./travel/spinnerloading.gif"),
 
-  ph1: require("./voting/ph1.webp"),
-  ph2: require("./voting/ph2.webp"),
 
   background: require("./travel/travelimage.jpg"),
   simpleimage:require("./travel/travelimage.jpg"),
